82ca4ca2b9ac66b5203abfd40299b312e0c051f8
Pavan-chegu <Pavanadityasairamchegu@gmail.com>
2025-06-28 16:57:34 +0530

    added for

--- test3.java
+++ test3.java
@@ -3,5 +3,8 @@ package tic_tac_toe;
 public class test3 {
     public static void main(String[] args) {
         System.out.println("test.main");
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
